cc4114d66109f9c8d4ca4918f6084b395a2208f3
danarocha <dana@danarocha.com.br>
2019-10-26 17:16:37 +0300

    feat: user model, migration and controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import './database';
 
 class App {
   constructor() {
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f13b602c4301a9a928517041168b6ada',
+  expiresIn: '7d',
+};
