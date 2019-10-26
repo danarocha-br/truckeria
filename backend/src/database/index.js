2dbeceb0918755326dd7606e534d49d9073c2644
danarocha <dana@danarocha.com.br>
2019-10-26 17:44:11 +0300

    feat: File model and controller

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 import dbConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
