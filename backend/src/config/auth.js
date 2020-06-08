00746d3c997c75032595d1ec2d30f2905856d3f6
Dana Rocha <dana.rocha@bigbank.ee>
2020-06-08 18:28:45 +0300

    init commit

--- backend/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: 'f13b602c4301a9a928517041168b6ada',
-  expiresIn: '7d',
-};
--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-const port = process.env.PORT || 3333;
-
-app.listen(port, () => console.log(`Listening on port ${port}.`));
