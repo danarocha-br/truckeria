f0c7b5a169f9d9215d86bec229b483735879364a
danarocha <dana@danarocha.com.br>
2020-06-13 11:01:35 +0300

    config: tailwind and babel

--- client/web/src/pages/Auth/styles.ts
+++ client/web/src/pages/Auth/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  ${tw`text-white text-base font-bold text-center bg-indigo-700 rounded-full flex py-4 px-10 focus:outline-none cursor-pointer`}
+`;
 
 export const Content = styled.div``;
 
--- client/web/src/tailwind.config.js
+++ client/web/src/tailwind.config.js
@@ -9,3 +9,4 @@ module.exports = {
   variants: {},
   plugins: [],
 };
+
