f4714b9dfa37ea984164ff464a90ce9017c6fc3b
Dana Rocha <dana@danarocha.com.br>
2020-10-08 19:09:12 +0300

    :sparkles: feat: server: ses config for mail

---
+++ server/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'equipe@truckeria.app',
+      name: 'Equipe Truckeria',
+    },
+  },
+} as IMailConfig;
---
+++ server/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,51 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
+import aws from 'aws-sdk';
+
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import mailConfig from '@config/mail';
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: 'us-east-2',
+      }),
+    });
+  }
+  /**
+   * sendEmail
+   */
+  public async sendEmail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parseTemplate(templateData),
+    });
+  }
+}
+
+export default SESMailProvider;
